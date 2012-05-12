1636ed77d0ce5c0f58cca5fb936d50de1a430d78
Øyvind Brungot Dahl <oyvindd@gmail.com>
2012-05-12 13:45:18 +0200

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,22 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
+
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-      					:text => "Ruby on Rails Tutorial Sample App | Home")
+      					:text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should have the custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+      					:text => "| Home")
     end
   end
 
@@ -20,10 +27,16 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-      					:text => "Ruby on Rails Tutorial Sample App | Help") 
+      					:text => "Ruby on Rails Tutorial Sample App") 
+    end
+
+    it "should have the custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title',
+      					:text => "| Help")
     end
   end
 
@@ -33,10 +46,17 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'About Us')
     end
   
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-    					:text => 'Ruby on Rails Tutorial Sample App | About Us')
+    					:text => 'Ruby on Rails Tutorial Sample App')
     end
+
+        it "should have the custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title',
+      					:text => "| About Us")
+    end
+
   end
 end
